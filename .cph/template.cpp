cc528e8263191adf08e61abcccfa756c6bd33c60
MohamedIrfanAM <i4irfan777@gmail.com>
2021-12-28 08:59:37 +0530

    updating template

--- .cph/template.cpp
+++ .cph/template.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
-#define coutc cout << "\033[48;5;196m\033[38;5;15m" 
-#define endlc "\033[0m" << endl; 
+#define cout_red cout << "\033[48;5;196m\033[38;5;15m" 
+#define endl_red "\033[0m" << endl; 
 using namespace std;
 
 

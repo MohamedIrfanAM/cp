fa701ede91d5ba7d27ff77b17b446b8ee164d332
Mohamed Irfan <i4irfan777@gmail.com>
2022-12-28 21:20:47 +0530

    codeforces round 837 div 2 & codechef staters 68

---
+++ codeforces/Round837Div.2/A_HossamandCombinatorics.cpp
@@ -0,0 +1,22 @@
+// time-limit: 2000
+// problem-url: https://codeforces.com/contest/1771/problem/A
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+int tst;
+cin >> tst;
+while(tst--)
+{
+  cout << "HELLOE" ;
+}
+
+return 0;
+}

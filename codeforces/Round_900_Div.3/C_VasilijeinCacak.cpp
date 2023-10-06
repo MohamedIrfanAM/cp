5eba0ddda7c99f33123bf54f2bc14f0849e784a1
Mohamed Irfan <i4irfan777@gmail.com>
2023-10-06 19:41:33 +0530

    practice problems

---
+++ codeforces/Round_900_Div.3/C_VasilijeinCacak.cpp
@@ -0,0 +1,105 @@
+// time-limit: 1000
+// problem-url: https://codeforces.com/contest/1878/problem/C
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int           long long int
+#define double        long double
+#define pb            push_back
+#define endl          "\n"
+#define all(p)        p.begin(), p.end()
+const int mod=1e9+7;
+
+#ifdef IRFAN_DEBUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) {cout << "\033[48;5;196m\033[38;5;15m" << name << " : " << arg1 << "\033[0m"<< endl;}
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr (names + 1, ',');
+  cout << "\033[48;5;196m\033[38;5;15m";cout.write (names, comma - names) << " : " << arg1 << " | " << "\033[0m"; __f (comma + 1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+
+int32_t main()
+{
+ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int tst;
+cin >> tst;
+while(tst--)
+{
+  int n,k,x;
+  cin >> n >> k >> x;
+  int sum = (k*(k+1))/2;
+  if(x < sum){
+    cout << "NO\n";
+    continue;
+  }
+
+  bool yes = false;
+  int l = k; 
+  
+  if((x-sum)/k <= n-l){
+     l += (x-sum)/k;
+     sum += k*((x-sum)/k);
+     if((n-l)*k < x-sum){
+       cout << "NO\n";
+     }
+     else{
+       cout << "YES\n";
+     }
+     // cout << x-sum << endl;
+
+  }
+  else{
+    cout << "NO\n";
+  }
+
+
+  // while(true){
+  //   // cout << sum << " " << x << " " << l << endl;
+  //   if(sum == x){
+  //      yes = true;
+  //      break;
+  //   }
+  //   else if(sum < x){
+  //     if(x-sum >= k){
+  //       if(l+1 <= n){
+  //         sum += k;
+  //         l++;
+  //       }
+  //       else break;
+  //     }
+  //     else{
+  //       if((n-l)*k >= x-sum){
+  //         yes = true;
+  //       }
+  //       break;
+  //     }
+  //   }
+  // }
+  // if(yes)cout << "YES\n";
+  // else cout << "NO\n";
+}
+
+return 0;
+}
+/*
+     ______      __             ______                             
+   _/      \_   |  \           /      \                            
+  /   $$$$$$ \   \$$  ______  |  $$$$$$\ ______   _______          
+ /  $$$____$$$\ |  \ /      \ | $$_  \$$|      \ |       \         
+|  $$/     \ $$\| $$|  $$$$$$\| $$ \     \$$$$$$\| $$$$$$$\        
+| $$|  $$$$$| $$| $$| $$   \$$| $$$$    /      $$| $$  | $$        
+| $$| $$| $$| $$| $$| $$      | $$     |  $$$$$$$| $$  | $$        
+| $$ \$$  $$| $$| $$| $$      | $$      \$$    $$| $$  | $$ ______ 
+ \$$\ \$$$$$$$$  \$$ \$$       \$$       \$$$$$$$ \$$   \$$|      \
+  \$$\ __/   \                                              \$$$$$$
+   \$$$    $$$                                                     
+     \$$$$$$ 
+*/

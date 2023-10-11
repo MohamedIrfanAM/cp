ede034249c9890a90e633e915a1d40b5280b6bb7
Mohamed Irfan <i4irfan777@gmail.com>
2023-10-11 07:34:46 +0530

    tempalte update, remove red colour from bug() because it doesn't format well

--- .utils/template.cpp
+++ .utils/template.cpp
@@ -12,11 +12,11 @@ const int mod=1e9+7;
 #ifdef IRFAN_DEBUG
 #define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
 template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) {cout << "\033[48;5;196m\033[38;5;15m" << name << " : " << arg1 << "\033[0m"<< endl;}
+void __f (const char* name, Arg1&& arg1) {cout << name << " : " << arg1 << endl;}
 template <typename Arg1, typename... Args>
 void __f (const char* names, Arg1&& arg1, Args&&... args){
   const char* comma = strchr (names + 1, ',');
-  cout << "\033[48;5;196m\033[38;5;15m";cout.write (names, comma - names) << " : " << arg1 << " | " << "\033[0m"; __f (comma + 1, args...);
+  cout.write (names, comma - names) << " : " << arg1 << " | " ; __f (comma + 1, args...);
 }
 #else
 #define bug(...)

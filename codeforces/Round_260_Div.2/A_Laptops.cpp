cdc165ea001a4e5531d49457110441198a3c9b08
Mohamed Irfan <i4irfan777@gmail.com>
2023-12-06 22:44:55 +0530

    practice problems

---
+++ codeforces/Round_260_Div.2/A_Laptops.cpp
@@ -0,0 +1,77 @@
+// time-limit: 1000
+// problem-url: https://codeforces.com/problemset/problem/456/A
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int           long long int
+#define double        long double
+#define pb            push_back
+#define endl          "\n"
+#define all(p)        p.begin(), p.end()
+#define fi            first
+#define se            second
+const int mod=1e9+7;
+
+#ifdef IRFAN_DEBUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) {cout << name << " : " << arg1 << endl;}
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr (names + 1, ',');
+  cout.write (names, comma - names) << " : " << arg1 << " | " ; __f (comma + 1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+
+void solve() 
+{
+  int n; cin >> n;
+  vector<pair<int,int>> v(n);
+  for(int i = 0; i < n; i++) 
+  {
+    cin >> v[i].fi >> v[i].se;
+  }
+  sort(all(v));
+  for(int i = 1; i < n; i++){
+    if(v[i].fi >= v[i-1].fi && v[i].se >= v[i-1].se){
+      continue;
+    }
+    else{
+      cout << "Happy Alex\n";
+      return;
+    }
+  }
+  cout << "Poor Alex\n";
+  
+}
+
+int32_t main()
+{
+ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int tst = 1;
+while(tst--)
+{
+  solve();
+}
+
+return 0;
+}
+/*
+     ______      __             ______                             
+   _/      \_   |  \           /      \                            
+  /   $$$$$$ \   \$$  ______  |  $$$$$$\ ______   _______          
+ /  $$$____$$$\ |  \ /      \ | $$_  \$$|      \ |       \         
+|  $$/     \ $$\| $$|  $$$$$$\| $$ \     \$$$$$$\| $$$$$$$\        
+| $$|  $$$$$| $$| $$| $$   \$$| $$$$    /      $$| $$  | $$        
+| $$| $$| $$| $$| $$| $$      | $$     |  $$$$$$$| $$  | $$        
+| $$ \$$  $$| $$| $$| $$      | $$      \$$    $$| $$  | $$ ______ 
+ \$$\ \$$$$$$$$  \$$ \$$       \$$       \$$$$$$$ \$$   \$$|      \
+  \$$\ __/   \                                              \$$$$$$
+   \$$$    $$$                                                     
+     \$$$$$$ 
+*/

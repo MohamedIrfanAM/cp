74f47443b686ff7b3d0bf8bb381226a5d4b7f377
Mohamed Irfan <i4irfan777@gmail.com>
2024-12-01 15:24:18 +0530

    AoC 24 day 1

---
+++ AoC2024/1b.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define double long double
+#define pb push_back
+#define endl "\n"
+#define all(p) p.begin(), p.end()
+#define fi first
+#define se second
+const int mod = 1e9 + 7;
+
+#ifdef IRFAN_DEBUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+  const char *comma = strchr(names + 1, ',');
+  cout.write(names, comma - names) << " : " << arg1 << " | ";
+  __f(comma + 1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+void solve()
+{
+  vector<int> a, b;
+  int x, y;
+  for (int i = 0; i < 1000; i++)
+  {
+    cin >> x >> y;
+    a.pb(x);
+    b.pb(y);
+  }
+  int ans = 0;
+  unordered_map<int, int> h;
+  for (int i = 0; i < a.size(); i++)
+  {
+    h[b[i]]++;
+  }
+  for (int i = 0; i < a.size(); i++)
+  {
+    ans += a[i] * h[a[i]];
+  }
+  cout << ans << "\n";
+}
+
+int32_t main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tst = 1;
+  // cin >> tst;
+  while (tst--)
+  {
+    solve();
+  }
+
+  return 0;
+}
+/*
+     ______      __             ______
+   _/      \_   |  \           /      \
+  /   $$$$$$ \   \$$  ______  |  $$$$$$\ ______   _______
+ /  $$$____$$$\ |  \ /      \ | $$_  \$$|      \ |       \
+|  $$/     \ $$\| $$|  $$$$$$\| $$ \     \$$$$$$\| $$$$$$$\
+| $$|  $$$$$| $$| $$| $$   \$$| $$$$    /      $$| $$  | $$
+| $$| $$| $$| $$| $$| $$      | $$     |  $$$$$$$| $$  | $$
+| $$ \$$  $$| $$| $$| $$      | $$      \$$    $$| $$  | $$ ______
+ \$$\ \$$$$$$$$  \$$ \$$       \$$       \$$$$$$$ \$$   \$$|      \
+  \$$\ __/   \                                              \$$$$$$
+   \$$$    $$$
+     \$$$$$$
+*/
